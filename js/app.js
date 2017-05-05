4bb849eb2abdd6284b757dffc80ec6d3b813c144
alexsanchezxc <alexsanchezxc@gmail.com>
2017-05-05 11:34:37 +0200

    cambios y seleccion de pelicula

--- js/app.js
+++ js/app.js
@@ -1,16 +1,14 @@
 var app = angular.module("imdb", ["ngRoute"]);
 
 app.controller("movieController", function($scope, $http) {
-  var imgPath = "https://image.tmdb.org/t/p/w300/";
-  this.movieSearch = "";
   this.formAction = function() {
     $http.get('https://api.themoviedb.org/3/search/movie?api_key=261f14ae26451d083908c559bf610fca&query=' + this.movieSearch).
     then(function(response) {
       $scope.movies = response.data.results;
     });
   };
-  this.movieSelect = function(imdbID) {
-    $http.get('https://api.themoviedb.org/3/search/movie?api_key=261f14ae26451d083908c559bf610fca&id=' + movieId).
+  this.movieSelect = function(id) {
+    $http.get('https://api.themoviedb.org/3/movie/' + id + '?api_key=261f14ae26451d083908c559bf610fca').
     then(function(data) {
       console.log(data.data);
       $scope.movie = data.data;
