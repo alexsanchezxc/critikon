3c37e49132ff4ffffb127ed63b1c9251237b6d5e
alexsanchezxc <alexsanchezxc@gmail.com>
2017-05-15 17:20:40 +0200

    cambios en la estructura de la pagina

--- pages/movie.php
+++ pages/movie.php
@@ -26,64 +26,7 @@
   <body>
     <div id="wrapper">
       <!-- NOTE: Navigation -->
-      <nav class="navbar navbar-inverse navbar-static-top" role="navigation" style="margin-bottom: 0">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-        </button>
-          <a class="navbar-brand" href="index.php">CRITIKON</a>
-        </div>
-        <!-- NOTE: navbar-header -->
-        <ul class="nav navbar-top-links navbar-right">
-          <li>
-            <div class="material-switch">
-              <input id="switch" name="switch" type="checkbox" />
-              <label for="switch" class="label-default"></label>
-            </div>
-          </li>
-          <li class="dropdown">
-            <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-            <i class="fa fa-user fa-fw"></i> <i class="fa fa-caret-down"></i>
-          </a>
-            <ul class="dropdown-menu dropdown-user">
-              <li><a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
-              </li>
-              <li><a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
-              </li>
-              <li class="divider"></li>
-              <li><a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
-              </li>
-            </ul>
-            <!-- NOTE: dropdown-user -->
-          </li>
-          <!-- NOTE: dropdown -->
-        </ul>
-        <!-- NOTE: navbar-top-links -->
-        <div class="sidebar" role="navigation">
-          <div class="sidebar-nav navbar-collapse">
-            <ul class="nav" id="side-menu">
-              <li class="sidebar-search">
-                <form method="get" class="input-group custom-search-form">
-                  <input type="text" class="form-control" name="movieSearch" placeholder="Buscar pelicula...">
-                  <span class="input-group-btn">
-                  <button class="btn btn-default" type="submit" name="boton">
-                    <i class="fa fa-search"></i>
-                  </button>
-                </span>
-                </form>
-                <!-- NOTE: input-group -->
-              </li>
-              <li>
-                <a href="index.php"><i class="fa fa-film fa-fw"></i> Inicio</a>
-              </li>
-            </ul>
-          </div>
-          <!-- NOTE: sidebar-collapse -->
-        </div>
-        <!-- NOTE: navbar-static-side -->
-      </nav>
+      <?php include('nav.php'); ?>
       <div id="page-wrapper">
         <?php
         $img = 'https://image.tmdb.org/t/p/original';
@@ -98,13 +41,13 @@
           echo '<!-- NOTE: col-lg-12 -->';
           echo '</div>';
           echo '<div class="row">';
-        	foreach($movies as $movie){
+          foreach($movies as $movie){
             echo '<a href="movie.php?id='.$movie->getID().'">';
             echo '<div class="movieList col-xs-6 col-sm-6 col-md-4 col-lg-3">';
             echo '<div class="thumbnail">';
             echo '<img class="poster" src="../assets/poster.png" alt="'.$movie->getTitle().'" style="background-image: url('.$img.$movie->getPoster().');">';
             echo '<div class="caption">';
-            echo '<a><h4 data-toggle="tooltip" data-placement="top" title="'.$movie->getTitle().'">'.$movie->getTitle().'</h4></a>';
+            echo '<h4 data-toggle="tooltip" data-placement="top" title="'.$movie->getTitle().'">'.$movie->getTitle().'</h4>';
             echo '<h6 data-toggle="tooltip" data-placement="bottom" title="'.$movie->get('release_date').'"><strong>Año:</strong> '.$movie->get('release_date').'</h6>';
             echo '</div>';
             echo '</div>';
---
+++ pages/nav.php
@@ -0,0 +1,63 @@
+<nav>
+  <div class="sidebar" role="navigation">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+    </div>
+    <!-- NOTE: navbar-header -->
+    <div class="sidebar-nav navbar-collapse">
+      <ul class="nav" id="side-menu">
+        <li>
+          <img class="img-responsive" src="">
+        </li>
+        <li class="sidebar-search">
+          <form method="get" class="input-group custom-search-form" autocomplete="off">
+            <input type="text" class="form-control" name="movieSearch" placeholder="Buscar pelicula..." required="">
+            <span class="input-group-btn">
+              <button class="btn btn-default" type="submit" name="boton">
+                <i class="fa fa-search"></i>
+              </button>
+            </span>
+          </form>
+          <!-- NOTE: input-group -->
+        </li>
+        <li>
+          <a href="index.php"><i class="fa fa-film fa-fw"></i> Inicio</a>
+        </li>
+      </ul>
+      <ul class="nav" id="side-menu">
+        <li>
+          <div class="material-switch">
+            <input id="switch" name="switch" type="checkbox" />
+            <label for="switch" class="label-default"></label>
+          </div>
+        </li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+            <i class="fa fa-user fa-fw"></i>
+            <i class="fa fa-caret-down"></i>
+          </a>
+          <ul class="dropdown-menu dropdown-user">
+            <li>
+              <a href="#"><i class="fa fa-user fa-fw"></i> User Profile</a>
+            </li>
+            <li>
+              <a href="#"><i class="fa fa-gear fa-fw"></i> Settings</a>
+            </li>
+            <li class="divider"></li>
+            <li>
+              <a href="login.php"><i class="fa fa-sign-out fa-fw"></i> Logout</a>
+            </li>
+          </ul>
+          <!-- NOTE: dropdown-user -->
+        </li>
+        <!-- NOTE: dropdown -->
+      </ul>
+    </div>
+    <!-- NOTE: sidebar-collapse -->
+  </div>
+  <!-- NOTE: navbar-static-side -->
+</nav>
